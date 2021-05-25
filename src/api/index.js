1dc8aa8c767d7a5165727513cd17a9304fe36046
AbigailXu <qbxzz233@gmail.com>
2021-05-25 04:27:13 -0400

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://studier-s.herokuapp.com/api',
+  baseURL: 'https://studier-s.herokuapp.com',
 })
 
-export const insertTask = task => api.post(`/task/task`, task)
-export const getAllTasks = () => api.get(`/task/tasks`)
-export const updateTaskById = (id, task) => api.put(`/task/task/${id}`, task)
-export const deleteTaskById = id => api.delete(`/task/task/${id}`)
-export const getTaskById = id => api.get(`/task/task/${id}`)
+export const insertTask = task => api.post(`/api/task/task`, task)
+export const getAllTasks = () => api.get(`/api/task/tasks`)
+export const updateTaskById = (id, task) => api.put(`/api/task/task/${id}`, task)
+export const deleteTaskById = id => api.delete(`/api/task/task/${id}`)
+export const getTaskById = id => api.get(`/api/task/task/${id}`)
 
-export const insertGenre = genre => api.post(`/genre/genre`, genre)
-export const getAllGenres = () => api.get(`/genre/genres`)
-export const updateGenreById = (id, genre) => api.put(`/genre/genre/${id}`, genre)
-export const deleteGenreById = id => api.delete(`/genre/genre/${id}`)
-export const getGenreById = id => api.get(`/genre/genre/${id}`)
+export const insertGenre = genre => api.post(`/api/genre/genre`, genre)
+export const getAllGenres = () => api.get(`/api/genre/genres`)
+export const updateGenreById = (id, genre) => api.put(`/api/genre/genre/${id}`, genre)
+export const deleteGenreById = id => api.delete(`/api/genre/genre/${id}`)
+export const getGenreById = id => api.get(`/api/genre/genre/${id}`)
 
 export const apiTask = {
     insertTask,
