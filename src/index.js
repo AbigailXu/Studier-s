a720329080639f4a2ce5f2b944b0d5c734e7c9ec
AbigailXu <qbxzz233@gmail.com>
2021-06-07 07:43:22 -0400

    beautify

--- src/index.js
+++ src/index.js
@@ -14,9 +14,10 @@ import App from "./App";
 
 // fontawsome
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
-library.add(far, fas);
+library.add(far, fas, fab);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 import BG1 from "../images/background1.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import area3 from "../images/3.png";
 import "../styles/common.css";
 import "../styles/cover.css";
@@ -9,33 +11,61 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <img className="bg" src={BG1} alt="background" />
-        <img
+        {/* <img
           onClick={() => window.open("https://github.com/AbigailXu/Studier-s")}
           className="area3"
           src={area3}
           alt="background"
-        />
+        /> */}
         <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+          <div className="bg-container">
+            <img className="bg" src={BG1} alt="background" />
+          </div>
           <div className="cover-s1">
             <h1>Studier's</h1>
-            <h3>- integral learning toolkit -</h3>
+            <h4>integrated learning toolkit</h4>
+            <button onClick={()=>this.props.history.push('/tasks/main')} 
+            className="badge rounded-pill start">Get Started</button>
           </div>
           <div className="cover-s2">
-            <h1>[ Currently avaliable features ]</h1>
-            
+            <h1>[ Current Features ]</h1>
+
             <h4>- Tasks management</h4>
             <h4>- Playing lofi music for a relaxing learning</h4>
           </div>
+        </div>
+        <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
           <div className="cover-s3">
-            <h3>
+            <div className="media-containers">
+              <h4 className="media-container-1">
+                <FontAwesomeIcon
+                  onClick={() =>
+                    window.open("https://github.com/AbigailXu/Studier-s")
+                  }
+                  className="media-icon"
+                  icon={["fab", "github"]}
+                />
+                <div>Frontend</div>
+                <div>Source Code</div>
+              </h4>
+              <h4 className="media-container-2">
+                <FontAwesomeIcon
+                  onClick={() =>
+                    window.open("https://github.com/AbigailXu/Studier-s-backend")
+                  }
+                  className="media-icon"
+                  icon={["fab", "github"]}
+                />
+                <div>Backend</div>
+                <div>Source Code</div>
+              </h4>
+            </div>
+            {/* <h3>
               Studier's is still under development, more features will be added.
-            </h3>
+            </h3> */}
           </div>
           <div className="cover-footer">
-            <p>
-            &copy; Copyright 2021, Abigail Xu
-            </p>
+            <p>&copy; Copyright 2021, Abigail Xu</p>
           </div>
         </div>
       </div>
